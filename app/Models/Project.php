046fb0a9b6285fe150ad2bb5a2abfb2b5edddb11
Yuri-Punzo <yuripunzo@hotmail.it>
2023-01-19 15:15:44 +0100

    initial commit

---
+++ app/Models/Project.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'description', 'type_id'];
+
+    public static function generateSlug($title)
+    {
+        $project_slug = Str::slug($title);
+        return $project_slug;
+    }
+
+    public function type(): BelongsTo
+    {
+        return $this->belongsTo(Type::class);
+    }
+}
